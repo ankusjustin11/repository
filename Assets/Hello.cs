1fabedbd42da9b453a9d89e554594a43a8e8e3ca
ankusjustin11 <jankus1@hawk.iit.edu>
2017-01-29 18:54:36 -0600

    Commit

---
+++ Assets/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Hello!");
+    }
